31c4236a20f10967834ff5a08a370ed7084cd60a
Hazwan <hazzwan12@gmail.com>
2021-10-24 14:59:53 +0800

    Commit report visitors 1st ver

--- Assigment1/reportVisitors.sh
+++ Assigment1/reportVisitors.sh
@@ -1,1 +1,13 @@
- > /tmp/visitors.txt
+#!/bin/bash
+
+## Get the list of users in visitor group
+visitorGroup=`cat /etc/groups | grep visitor`
+
+## Remove the Group details but splitting by ':' delim and extract users
+users=`echo $visitorGroup | cut -d ':' -f 4`
+
+## Replace comma delimeter with \\n so that each user is seperated by a new line
+results=`echo $users | tr , \\n`
+
+## Dump results into visitors.txt
+$results > /tmp/visitors.txt
