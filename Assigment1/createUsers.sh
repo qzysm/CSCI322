3700f36f35599bcd6daf7cf66744f6f4b1fe76ff
Hazwan <hazzwan12@gmail.com>
2021-10-24 13:40:25 +0800

    Bugfixes to handle carriage returns

--- Assigment1/createUsers.sh
+++ Assigment1/createUsers.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+
+filename='Usernames.txt'
+
 ## Create group visitors and staff
 logger Creating new group visitor
 sudo addgroup visitor
 logger Creating new group staff
 sudo addgroup staff
+
+## Remove any carriage return in file
+sed 's/\r$//' $filename > tmp.txt
 
 logger Parsing Usernames.txt to create Users
 ## Create User with password same as username, home directory and group
@@ -11,7 +17,10 @@ while IFS="," read -r username group; do
     logger Creating User $username and adding to Group $group
     sudo useradd $username -m -s /bin/bash -G $group
     echo "$username\n$username" | passwd $username
-done < 'Usernames.txt'
+done < 'tmp.txt'
 unset IFS
 
 logger User Creation Completed
+
+## Delete tmp file that was created earlier
+rm tmp.txt
