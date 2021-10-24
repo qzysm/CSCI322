204f921b3ed9968532634a2dacbf5d0f0554979c
Hazwan <hazzwan12@gmail.com>
2021-10-24 15:18:27 +0800

    New way of dumping visitors.txt

--- Assigment1/reportVisitors.sh
+++ Assigment1/reportVisitors.sh
@@ -6,8 +6,6 @@ visitorGroup=`cat /etc/group | grep visitor`
 ## Remove the Group details but splitting by ':' delim and extract users
 users=`echo $visitorGroup | cut -d ':' -f 4`
 
-## Replace comma delimeter with \\n so that each user is seperated by a new line
-results=`echo $users | tr , \\n`
-
 ## Dump results into visitors.txt
-echo $results > /tmp/visitors.txt
+IFS=',' read -ra user <<< $users
+printf '%s\n' "$user[@]" > /tmp/visitors.txt
