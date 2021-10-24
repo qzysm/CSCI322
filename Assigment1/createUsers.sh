e035b50c2a8d7256108c264ec8e263d1a9c0665e
Hazwan <hazzwan12@gmail.com>
2021-10-24 13:12:28 +0800

    Remove Echo in commands

--- Assigment1/createUsers.sh
+++ Assigment1/createUsers.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 ## Create group visitors and staff
 logger Creating new group visitor
-echo "sudo addgroup visitor"
+sudo addgroup visitor
 logger Creating new group staff
-echo "sudo addgroup staff"
+sudo addgroup staff
 
 logger Parsing Usernames.txt to create Users
 ## Create User with password same as username, home directory and group
 while IFS="," read -r username group; do
     logger Creating User $username and adding to Group $group
-    echo "sudo useradd $username -m -s /bin/bash -G $group"
-    echo "$username:$username | sudo chpasswd"
+    sudo useradd $username -m -s /bin/bash -G $group
+    echo "$username:$username" | sudo chpasswd
 done < 'Usernames.txt'
 unset IFS
 
