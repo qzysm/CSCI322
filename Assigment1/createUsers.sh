38d250689bfea595bd3a0f925fcd2d733b220d39
Hazwan <hazzwan12@gmail.com>
2021-10-24 13:09:39 +0800

    Create users

--- Assigment1/createUsers.sh
+++ Assigment1/createUsers.sh
@@ -1,9 +1,17 @@
-# /bin/bash
-
+#!/bin/bash
 ## Create group visitors and staff
-sudo addgroup visitors
-sudo addgroup staff
+logger Creating new group visitor
+echo "sudo addgroup visitor"
+logger Creating new group staff
+echo "sudo addgroup staff"
 
+logger Parsing Usernames.txt to create Users
 ## Create User with password same as username, home directory and group
-sudo useradd -m -g $groupname $username
+while IFS="," read -r username group; do
+    logger Creating User $username and adding to Group $group
+    echo "sudo useradd $username -m -s /bin/bash -G $group"
+    echo "$username:$username | sudo chpasswd"
+done < 'Usernames.txt'
+unset IFS
 
+logger User Creation Completed
